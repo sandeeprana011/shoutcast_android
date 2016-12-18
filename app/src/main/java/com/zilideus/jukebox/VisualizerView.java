07ecc1e0ff36ebab571fc471dfe8ada44a0e7b02
sandeeprana <sandeep@loktra.com>
2016-12-18 12:46:01 +0530

    Added New Style to equalizer TYPE_BAR

--- app/src/main/java/com/zilideus/jukebox/VisualizerView.java
+++ app/src/main/java/com/zilideus/jukebox/VisualizerView.java
@@ -10,10 +10,18 @@ import android.view.View;
 
 public class VisualizerView extends View {
 
+    public static final int TYPE_BAR = 0;
+    public static final int TYPE_LINES = 1;
+
+
+    public static int typeVisualizer;
+
+    Rect rect = new Rect();
     private byte[] mBytes;
     private float[] mPoints;
     private Rect mRect = new Rect();
     private Paint mForePaint = new Paint();
+    private Rect[] rects = new Rect[100];
 
     public VisualizerView(Context context) {
         super(context);
@@ -45,23 +53,53 @@ public class VisualizerView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
         if (mBytes == null) {
             return;
         }
         if (mPoints == null || mPoints.length < mBytes.length * 4) {
             mPoints = new float[mBytes.length * 4];
         }
-        mRect.set(0, 0, getWidth(), getHeight());
-        for (int i = 0; i < mBytes.length - 1; i++) {
-            mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
-            mPoints[i * 4 + 1] = mRect.height() / 2
-                    + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
-            mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
-            mPoints[i * 4 + 3] = mRect.height() / 2
-                    + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2)
-                    / 128;
+
+        if (typeVisualizer == VisualizerView.TYPE_BAR) {
+
+            mRect.set(0, 0, getWidth(), getHeight());
+            int widthBetweenBuildings = 1;
+            int noOfBuildings = 20;
+
+            int widthRects = ((getWidth() - widthBetweenBuildings + 1) / noOfBuildings);
+
+            int leftOffset = 0;
+            int rightOffset = 0;
+
+            for (int i = 0; i < noOfBuildings; i++) {
+                leftOffset = (i * widthRects) + i;
+                rightOffset = ((i * widthRects) + i) + widthRects;
+
+                int alora = mRect.height() / 2
+                        + ((byte) (mBytes[i * (mBytes.length / noOfBuildings)] + 128)) * (mRect.height() / 2) / 128;
+
+                rect.set(leftOffset, 0, rightOffset, alora);
+
+
+//            rects[i] = rect;
+                canvas.drawRect(new Rect(rect), mForePaint);
+            }
+        } else if (typeVisualizer == VisualizerView.TYPE_LINES) {
+
+            for (int i = 0; i < mBytes.length - 1; i++) {
+                mPoints[i * 4] = mRect.width() * i / (mBytes.length - 1);
+                mPoints[i * 4 + 1] = mRect.height() / 2
+                        + ((byte) (mBytes[i] + 128)) * (mRect.height() / 2) / 128;
+                mPoints[i * 4 + 2] = mRect.width() * (i + 1) / (mBytes.length - 1);
+                mPoints[i * 4 + 3] = mRect.height() / 2
+                        + ((byte) (mBytes[i + 1] + 128)) * (mRect.height() / 2)
+                        / 128;
+            }
+            canvas.drawLines(mPoints, mForePaint);
         }
-        canvas.drawLines(mPoints, mForePaint);
+        canvas.rotate(180);
     }
 
 }
